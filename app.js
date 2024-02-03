02dbf82cee3b2e8140974e1d76935e76c75825cc
oghenrique <santosguhenrique@gmail.com>
2024-02-03 18:42:18 -0300

    correcao de cores e backgrounds aleatorios

--- app.js
+++ app.js
@@ -16,3 +16,16 @@ showPasswordIcon.addEventListener('click', function () {
         showPasswordIcon.classList.remove('blinking')
     }, 1000)
 })
+
+
+//Background aleatorios
+var backgrounds = [
+    'url(../img/background.svg)',
+    'url(../img/background-2.webp)',
+    'url(../img/background-3.jpg)',
+]
+var selectedBackground = backgrounds[Math.floor(Math.random() * backgrounds.length)]
+
+document.body.style.backgroundImage = selectedBackground;
+
+  
