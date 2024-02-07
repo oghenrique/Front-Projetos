c7af61dba7af1f41bcf431a58d5032292155cacf
oghenrique <santosguhenrique@gmail.com>
2024-02-07 09:55:49 -0300

    tarefas

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,18 @@
 'use strict'
 
+//Background aleatorios
+var backgrounds = [
+    'url(../img/background.svg)',
+    'url(../img/background-2.webp)',
+    'url(../img/background-3.jpg)',
+]
+
+
+var selectedBackground = backgrounds[Math.floor(Math.random() * backgrounds.length)]
+
+document.body.style.backgroundImage = selectedBackground;
+
+
 var showPasswordIcon = document.getElementById('show-password')
 var passwordField = document.getElementById('pass')
 
@@ -17,15 +30,5 @@ showPasswordIcon.addEventListener('click', function () {
     }, 1000)
 })
 
-
-//Background aleatorios
-var backgrounds = [
-    'url(../img/background.svg)',
-    'url(../img/background-2.webp)',
-    'url(../img/background-3.jpg)',
-]
-var selectedBackground = backgrounds[Math.floor(Math.random() * backgrounds.length)]
-
-document.body.style.backgroundImage = selectedBackground;
 
   
--- assets/tarefas.js
+++ assets/tarefas.js
@@ -1,14 +1,101 @@
-'use strict'
+// 'use strict'
 
-const userId = sessionStorage.getItem('userId')
+// const userId = sessionStorage.getItem('userId')
 
-async function obterTarefas() {
-    const url = `http://127.0.0.1:5080/tarefas?id=${userId}`
+// async function obterTarefas() {
+//     const url = `http://127.0.0.1:5080/tarefas?id=${userId}`
 
-    const response = await fetch(url)
-    const tarefas = await response.json()
+//     const response = await fetch(url)
+//     const tarefas = await response.json()
 
-    console.log(tarefas)
+//     console.log(tarefas)
+// }
+
+// obterTarefas()
+
+const button = document.querySelector('.button-add-task')
+const input = document.querySelector('.input-task')
+const listaCompleta = document.querySelector('.list-tasks')
+
+let minhaListaDeItens = []
+
+function adicionarNovaTarefa() {
+    minhaListaDeItens.push({
+        tarefa: input.value,
+        concluida: false,
+    })
+
+    input.value = ''
+
+    mostrarTarefas()
 }
 
-obterTarefas()
+function mostrarTarefas() {
+    let novaLi = ''
+    minhaListaDeItens.forEach((item, posicao) => {
+      novaLi +=
+        `<li class="task ${item.concluida ? 'done' : ''}">
+          <div class="circle checked" onclick="concluirTarefa(${posicao})"></div>
+          <p contenteditable="${item.editando ? 'true' : 'false'}" 
+             onblur="finalizarEdicao(${posicao}, this.innerText)"
+             onkeydown="verificarEnter(event, ${posicao})">${item.tarefa}</p>
+          <div class="edit-container">
+              <div class="circle edit" onclick="editarItem(${posicao})"></div>
+              <div class="circle trash" onclick="deletarItem(${posicao})"></div>
+          </div>
+        </li>`
+    })
+    listaCompleta.innerHTML = novaLi
+    localStorage.setItem('lista', JSON.stringify(minhaListaDeItens))
+  }
+  
+  function verificarEnter(event, posicao) {
+    if (event.key === 'Enter') {
+      event.preventDefault()
+      finalizarEdicao(posicao, event.target.innerText)
+    }
+  }
+  
+
+function editarItem(posicao) {
+    minhaListaDeItens = minhaListaDeItens.map((item, index) => {
+        if (index === posicao) {
+            return { ...item, editando: true }
+        }
+        return item
+    })
+    mostrarTarefas()
+    document.querySelectorAll('.list-tasks .task p')[posicao].focus()
+}
+
+function finalizarEdicao(posicao, novoTexto) {
+    minhaListaDeItens[posicao].tarefa = novoTexto
+    minhaListaDeItens[posicao].editando = false
+    mostrarTarefas()
+}
+
+function concluirTarefa(posicao) {
+    minhaListaDeItens[posicao].concluida = !minhaListaDeItens[posicao].concluida
+
+    mostrarTarefas()
+}
+
+function deletarItem(posicao) {
+    minhaListaDeItens.splice(posicao, 1)
+
+    mostrarTarefas()
+}
+
+
+function recarregarTarefas() {
+    const tarefasDoLocalStorage = localStorage.getItem('lista')
+
+    if (tarefasDoLocalStorage) {
+        minhaListaDeItens = JSON.parse(tarefasDoLocalStorage)
+    }
+
+    mostrarTarefas()
+}
+
+recarregarTarefas()
+button.addEventListener('click', adicionarNovaTarefa)
