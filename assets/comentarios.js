ec7f060264050262cec316e17d4b7db7e7217b61
NPS <nicolaspetri18@gmail.com>
2024-03-07 16:49:38 -0300

    aaaaaaaaaaa q saco

--- assets/comentarios.js
+++ assets/comentarios.js
@@ -4,21 +4,29 @@ const inputName = document.getElementById('inputTitulo')
 const textComment = document.getElementById('inputText')
 const form = document.getElementById('formulario')
 const commentPost = document.getElementById('commentPost')
+const idTarefas = sessionStorage.getItem('idTarefas')
 
 form.addEventListener('submit', (event) => {
+    event.preventDefault();
 
-    event.preventDefault()
+    const inputName = document.getElementById('inputTitulo');
+    const textComment = document.getElementById('inputText');
+    const commentPost = document.getElementById('commentPost');
 
+    adicionarComentario(inputName.value, textComment.value, idTarefas);
+
+    inputName.value = "";
+    textComment.value = "";
+
+    inputName.focus();
+});
+
+function adicionarComentario(nome, comentario, idTarefas) {
     let p = document.createElement('p')
     p.classList = 'p-2 d-flex text-wrap flex-wrap'
     p.id = 'comentario'
-    p.innerHTML = `<strong>${inputTitulo.value}: </strong> &nbsp${textComment.value}`
+    p.innerHTML = `<strong>${nome}: </strong> &nbsp${comentario} &nbsp${idTarefas}`
 
     commentPost.appendChild(p)
-    inputTitulo.value = ""
-    textComment.value = ""
-
-    inputTitulo.focus()
-
-
-})
+    console.log('ID da Tarefa:', idTarefas);
+}
